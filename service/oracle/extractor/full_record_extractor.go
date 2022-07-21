e25b3185de98b53d1ab5bd73fd0f8fca6bb6858c
spiswe <spiswe.mk@outlook.com>
2022-07-21 19:00:12 +0800

    add continue to develop extractor

--- service/oracle/extractor/full_record_extractor.go
+++ service/oracle/extractor/full_record_extractor.go
@@ -1,13 +1,52 @@
 package extractor
 
-import "go-mysql-transfer/service/oracle/models"
+import (
+	"fmt"
+	"go-mysql-transfer/service/oracle"
+	"go-mysql-transfer/service/oracle/models"
+	meta "go-mysql-transfer/service/oracle/oracle_meta"
+	"strings"
+)
 
 const (
-	FORMAT        string = "select * from (select {0} from {1}.{2} t where {3} > ? order by {3} asc) where rownum <= ?"
-	MIN_PK_FORMAT string = "select min({0}) from {1}.{2}"
+	FORMAT        string = "select * from (select %s from %s.%s t where %s > ? order by {3} asc) where rownum <= ?"
+	MIN_PK_FORMAT string = "select min(%s) from {%s}.{%s}"
 )
 
 type OracleFullRecordExtractor struct {
 	absOraRecExt  AbstractOracleRecordExtractor
 	oracleContext models.OracleContext
+	queue         chan models.Record
+	extractSQL    string
+	extractThread oracle.Pool
+	getMinPkSQL   string
+}
+
+func NewOracleFullRecordExtractor(oracleContext models.OracleContext) *OracleFullRecordExtractor {
+	return &OracleFullRecordExtractor{oracleContext: oracleContext}
+}
+
+func (o *OracleFullRecordExtractor) start() {
+	extractor := o.absOraRecExt.Extractor()
+	extractor.Start()
+	tableMeta := o.oracleContext.TableMeta()
+	primaryKey := tableMeta.PrimaryKeys()[0].Name()
+	schemaName := tableMeta.Schema()
+	tableName := tableMeta.Name()
+
+	if o.extractSQL == "" {
+		columnWithPk := tableMeta.GetColumnsWithPrimary()
+		colString := meta.SQLTemplateControl.MakeColumn(columnWithPk)
+		o.extractSQL = fmt.Sprintf(FORMAT, colString, schemaName, tableName, primaryKey)
+	}
+
+	if o.getMinPkSQL == "" && strings.Trim(primaryKey, " ") != "" {
+		o.getMinPkSQL = fmt.Sprintf(MIN_PK_FORMAT, primaryKey, schemaName, tableName)
+	}
+
+}
+
+func (o *OracleFullRecordExtractor) ContinueExtractor(context models.OracleContext) {
+	position := context.LastPosition()
+
 }
--- service/oracle/models/oracle_context.go
+++ service/oracle/models/oracle_context.go
@@ -7,24 +7,24 @@ import (
 )
 
 type OracleContext struct {
-	lastPosition         positioner.AbstractPosition // 最后一次同步的position 记录
-	tableMeta            meta.Table                  // 对应table oracle_meta
-	ignoreSchema         bool                        // 设置是否忽略schema
-	runMode              RunMode                     // 运行模式
-	onceCrawNum          int                         // 单次提取记录数量
-	tpsLimit             int                         `default:"0"` // 0 及以下代表不限制
-	sourceDS             database.DataSource         // datasource
-	targetDS             database.DataSource         // target datasource
-	batchApply           bool                        // 是否批量
-	skipApplierException bool                        // 是否跳过异常
-	SourceCodeEncoding   string                      `default:"UTF-8"` //source encoding
-	TargetCodeEncoding   string                      `default:"UTF-8"` // target encoding
-	mViewLogType         string                      // 物化视图日志类型
-	tablePKs             map[string][]string         // pk
+	lastPosition         positioner.OraclePosition // 最后一次同步的position 记录
+	tableMeta            meta.Table                // 对应table oracle_meta
+	ignoreSchema         bool                      // 设置是否忽略schema
+	runMode              RunMode                   // 运行模式
+	onceCrawNum          int                       // 单次提取记录数量
+	tpsLimit             int                       `default:"0"` // 0 及以下代表不限制
+	sourceDS             database.DataSource       // datasource
+	targetDS             database.DataSource       // target datasource
+	batchApply           bool                      // 是否批量
+	skipApplierException bool                      // 是否跳过异常
+	SourceCodeEncoding   string                    `default:"UTF-8"` //source encoding
+	TargetCodeEncoding   string                    `default:"UTF-8"` // target encoding
+	mViewLogType         string                    // 物化视图日志类型
+	tablePKs             map[string][]string       // pk
 }
 
 func NewOracleContextFull(
-	lastPosition positioner.AbstractPosition,
+	lastPosition positioner.OraclePosition,
 	tableMeta meta.Table,
 	ignoreSchema bool,
 	runMode RunMode,
@@ -99,7 +99,11 @@ func (o *OracleContext) MViewLogType() string {
 	return o.mViewLogType
 }
 
-func (o *OracleContext) SetLastPosition(position positioner.AbstractPosition) {
+func (o *OracleContext) LastPosition() positioner.OraclePosition {
+	return o.lastPosition
+}
+
+func (o *OracleContext) SetLastPosition(position positioner.OraclePosition) {
 	o.lastPosition = position
 }
 
